856325d826fc38190c14e2a14084c13106106fb8
Harsh Dhadwal <harsh.dhadwal@my.jcu.edu.au>
2022-03-25 10:30:44 +1000

    rounding up values

--- app/src/main/java/au/jcu/edu/tipcalculatorv2/BillCalculator.java
+++ app/src/main/java/au/jcu/edu/tipcalculatorv2/BillCalculator.java
@@ -9,8 +9,8 @@ public class BillCalculator {
     void calculate(float billAmount, int tipPercent, int splitNum){
 
         float tipAmount = billAmount * tipPercent / 100;
-        this.totalAmount = billAmount + tipAmount;
-        this.perPersonAmount = totalAmount / splitNum;
+        this.totalAmount = (float) Math.ceil((billAmount + tipAmount)*10)/10;
+        this.perPersonAmount = (float) (Math.ceil((totalAmount / splitNum)*10)/10);
     }
 
     float getTotalAmount(){
